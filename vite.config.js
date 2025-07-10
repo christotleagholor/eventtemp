77abde52a56b97715b0907a604f260df57843151
christotleagholor <christotleagholor@gmail.com>
2025-07-10 13:29:32 +0100

    vercel.json file adjusted

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: '/', // ← important for Vercel deployment
-  build: {
-    outDir: 'dist'
-  }
-})
+  
+  })
